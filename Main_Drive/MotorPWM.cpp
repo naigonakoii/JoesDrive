50d81f38c4e9707d021f7d2b7c61500a5ee35a48
naigonakoii <naigonakoii@gmail.com>
2020-09-25 22:46:46 -0700

    Convert Motor PWM to 4 spaces instead of 2

--- Main_Drive/MotorPWM.cpp
+++ Main_Drive/MotorPWM.cpp
@@ -18,37 +18,37 @@ namespace Naigon::BB_8
 {
 
 MotorPWM::MotorPWM(int pwmPin1, int pwmPin2, int maxInput, int minOutput)
-  : _pwmPin1(pwmPin1)
-  , _pwmPin2(pwmPin2)
-  , _maxInput(maxInput)
-  , _minOutput(minOutput)
+    : _pwmPin1(pwmPin1)
+    , _pwmPin2(pwmPin2)
+    , _maxInput(maxInput)
+    , _minOutput(minOutput)
 {
 }
 
 void MotorPWM::WritePWM(int output, int input)
 {
-  if (output <= -_minOutput
-    && (input > -_maxInput || _maxInput == 0))
-  {
-    analogWrite(_pwmPin1, 0);
-    analogWrite(_pwmPin2, abs(output));
-  }
-  else if (output >= _minOutput
-    && (input < _maxInput || _maxInput == 0))
-  {
-    analogWrite(_pwmPin2, 0);
-    analogWrite(_pwmPin1, abs(output));
-  }
-  else
-  {
-    WriteZeros();
-  }
+    if (output <= -_minOutput
+        && (input > -_maxInput || _maxInput == 0))
+    {
+        analogWrite(_pwmPin1, 0);
+        analogWrite(_pwmPin2, abs(output));
+    }
+    else if (output >= _minOutput
+        && (input < _maxInput || _maxInput == 0))
+    {
+        analogWrite(_pwmPin2, 0);
+        analogWrite(_pwmPin1, abs(output));
+    }
+    else
+    {
+        WriteZeros();
+    }
 }
 
 void MotorPWM::WriteZeros()
 {
-  analogWrite(_pwmPin1, 0);
-  analogWrite(_pwmPin2, 0);
+    analogWrite(_pwmPin1, 0);
+    analogWrite(_pwmPin2, 0);
 }
 
 } // namespace Naigon::BB_8
